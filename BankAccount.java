91e7b87b94494a0ef1254d1e58daf99505f19fde
emmachio <emmany2409@gmail.com>
2020-10-29 00:15:59 -0400

    get AccountID

---
+++ BankAccount.java
@@ -0,0 +1,21 @@
+public class BankAccount {
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(int a, String p) {
+    balance = 0;
+    accountID = a;
+    password = p;
+  }
+
+  public String getBalance() {
+    return("balance is" + balance);
+  }
+
+  public String getAccountID() {
+    return("AccountID is" + accountID);
+  }
+
+
+}
