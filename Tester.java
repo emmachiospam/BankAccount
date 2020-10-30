1f4de2c6f0e99a5cd587f27058fcde3e878a6a35
emmachio <emmany2409@gmail.com>
2020-10-30 18:58:35 -0400

    updated

--- Tester.java
+++ Tester.java
@@ -1,11 +1,22 @@
 public class Tester {
   public static void main(String[] args) {
-    BankAccount b1 = new BankAccount(12345, "hullo");
-    b1.deposit(500);
-    b1.deposit(500);
-    b1.deposit(500);
-    b1.withdraw(1600);
+    BankAccount b1 = new BankAccount(1000001, "abc123");
+    BankAccount b2 = new BankAccount(1000231, "abcasdg23");
+    b1.deposit(3000.0);
+    b2.deposit(1234.0);
     System.out.println(b1.getBalance());
-    System.out.println(b1.toString());
+    System.out.println(b2.getBalance());
+    System.out.println(b1.transferTo(b2, 200.0, "no"));
+    System.out.println(b1.getBalance());
+    System.out.println(b2.getBalance());
+
+
+    // b1.deposit(500);
+    // b1.deposit(500);
+    // b1.deposit(500);
+    // b1.withdraw(1600);
+    // b1.setPassword("testing");
+    // System.out.println(b1.getBalance());
+    // System.out.println(b1.toString());
   }
 }
